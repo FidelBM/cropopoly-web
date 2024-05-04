a1a8c4db89132357889619394ab4f25fb1cce6f1
FidelBM <133674887+FidelBM@users.noreply.github.com>
2024-05-04 13:04:07 -0600

    Update pagina.jsx

--- src/pagina.jsx
+++ src/pagina.jsx
@@ -1,43 +1,44 @@
-import { useState } from 'react'
-import backgroundImage from './assets/Images/Fondo.png'; // Asegúrate de reemplazar esto con la ruta a tu imagen
+import { useState } from 'react'; // Importa la función useState desde React
+import backgroundImage from './assets/Images/Fondo.png'; // Importa la imagen de fondo
+import './App.css'; // Importa los estilos CSS
+import { FirstComponent } from './assets/Components/FirstComponent'; // Importa el primer componente
+import { GameComponent } from './assets/Components/GameComponent'; // Importa el componente de juego
+import { RanStatesComponent } from './assets/Components/RanStatesComponent'; // Importa el componente RanStates
+import { MurcsComponent } from './assets/Components/MurcsComponent'; // Importa el componente Murcs
+import { TextCropopoly } from './assets/Components/TextCropopoly'; // Importa el componente TextCropopoly
+import { DisplayFotos } from './assets/Components/DisplayFotos'; // Importa el componente DisplayFotos
+import { GraciasPorJugar } from './assets/Components/GraciasPorJugar'; // Importa el componente GraciasPorJugar
+import { PruebaComments } from './assets/Components/PruebaComments'; // Importa el componente PruebaComments
+import { TutorialComponent } from './assets/Components/TutorialComponent'; // Importa el componente TutorialComponent
+import { TrailerComponent } from './assets/Components/TrailerComponent'; // Importa el componente TrailerComponent
 
-import './App.css'
-import {FirstComponent} from './assets/Components/FirstComponent';
-import { GameComponent } from './assets/Components/GameComponent';  
-import { RanStatesComponent } from './assets/Components/RanStatesComponent';
-import {MurcsComponent} from './assets/Components/MurcsComponent';
-import {TextCropopoly} from './assets/Components/TextCropopoly';
-import {DisplayFotos} from './assets/Components/DisplayFotos';
-import {GraciasPorJugar} from './assets/Components/GraciasPorJugar';
-import {PruebaComments} from './assets/Components/PruebaComments';
-import { TutorialComponent } from './assets/Components/TutorialComponent';
-import { TrailerComponent } from './assets/Components/TrailerComponent';
-
-
+// Define la función Pagina
 function Pagina() {
+  // Estilo para el fondo de la página
   const appStyle = {
-    backgroundImage: `url(${backgroundImage})`,
+    backgroundImage: `url(${backgroundImage})`, // Fondo de la imagen importada
     backgroundSize: 'cover', // Esto hará que la imagen cubra todo el elemento
     backgroundRepeat: 'no-repeat', // Esto evitará que la imagen se repita
   };
-  
 
+  // Retorna el contenido de la página
   return (
-    <div className="w-full h-full" style={appStyle}>
+    <div className="w-full h-full" style={appStyle}> {/* Contenedor principal con estilo de fondo */}
       <header className="App-header w-full">
-        <FirstComponent className="mb-8" />
-        <GameComponent className="mb-8" />
-        <RanStatesComponent className="mb-8" />
-        <MurcsComponent className="mb-8" />
-        <TutorialComponent className="mb-8"/>
-        <TrailerComponent className="mb-8"/>
-        <TextCropopoly className="mb-8" />
-        <DisplayFotos className="mb-8" />
-        <GraciasPorJugar className="mb-8" />
-        <PruebaComments className="mb-8" />
+        {/* Renderiza los diferentes componentes */}
+        <FirstComponent className="mb-8" /> {/* Primer componente */}
+        <GameComponent className="mb-8" /> {/* Componente de juego */}
+        <RanStatesComponent className="mb-8" /> {/* Componente RanStates */}
+        <MurcsComponent className="mb-8" /> {/* Componente Murcs */}
+        <TutorialComponent className="mb-8"/> {/* Componente de tutorial */}
+        <TrailerComponent className="mb-8"/> {/* Componente de trailer */}
+        <TextCropopoly className="mb-8" /> {/* Componente TextCropopoly */}
+        <DisplayFotos className="mb-8" /> {/* Componente DisplayFotos */}
+        <GraciasPorJugar className="mb-8" /> {/* Componente GraciasPorJugar */}
+        <PruebaComments className="mb-8" /> {/* Componente PruebaComments */}
       </header>
     </div>
-  )
+  );
 }
 
-export default Pagina
+export default Pagina; // Exporta la función Pagina como componente principal
