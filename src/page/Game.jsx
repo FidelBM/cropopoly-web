fb9489c6402979d7c45b09feed4af3567b034f6b
FidelBM <133674887+FidelBM@users.noreply.github.com>
2024-05-04 13:09:24 -0600

    Update Game.jsx

--- src/page/Game.jsx
+++ src/page/Game.jsx
@@ -1,104 +1,81 @@
-import { useState, useEffect, Fragment, useContext } from 'react';
-import { Unity, useUnityContext } from "react-unity-webgl";
-import { Link } from "react-router-dom";
+import { useState, useEffect, Fragment, useContext } from 'react'; // Importa useState, useEffect, Fragment y useContext desde React
+import { Unity, useUnityContext } from "react-unity-webgl"; // Importa Unity y useUnityContext desde react-unity-webgl
+import { Link } from "react-router-dom"; // Importa Link desde React Router DOM
 
-import { authContext } from "../context/authContext";
+import { authContext } from "../context/authContext"; // Importa el contexto de autenticación
 
 function Game() {
   
-  const { user, logout } = useContext(authContext); // Obtén logout de authContext
-  const [userData, setUserData] = useState(null);
+  const { user, logout } = useContext(authContext); // Obtén user y logout del contexto de autenticación
+  const [userData, setUserData] = useState(null); // Estado para almacenar los datos del usuario
 
   useEffect(() => {
-    if (user) {
-      fetch(`https://cropopoly-server-production.up.railway.app/jugadores?email=${user.email}`)
+    if (user) { // Verifica si hay un usuario autenticado
+      fetch(`https://cropopoly-server-production.up.railway.app/jugadores?email=${user.email}`) // Realiza una solicitud HTTP para obtener los datos del usuario
         .then(response => response.json())
         .then(data => {
           if (data.length > 0) {
-            setUserData(data[0]);
+            setUserData(data[0]); // Almacena los datos del usuario en el estado
           }
         })
         .catch(error => console.error(error));
     }
   }, [user]);
 
-  
-
-  console.log(user.email)
-
-  const [containerSize, setContainerSize] = useState({ width: window.innerWidth, height: window.innerHeight });
-  const { unityProvider } = useUnityContext({
-    loaderUrl: "./src/page/WebGL/Build/WebGL.loader.js",
-    dataUrl: "./src/page/WebGL/Build/WebGL.data",
-    frameworkUrl: "./src/page/WebGL/Build/WebGL.framework.js",
-    codeUrl: "./src/page/WebGL/Build/WebGL.wasm",
+  const [containerSize, setContainerSize] = useState({ width: window.innerWidth, height: window.innerHeight }); // Estado para almacenar el tamaño del contenedor
+  const { unityProvider } = useUnityContext({ // Obtiene el proveedor de Unity
+    loaderUrl: "./src/page/WebGL/Build/WebGL.loader.js", // URL del cargador
+    dataUrl: "./src/page/WebGL/Build/WebGL.data", // URL de los datos
+    frameworkUrl: "./src/page/WebGL/Build/WebGL.framework.js", // URL del framework
+    codeUrl: "./src/page/WebGL/Build/WebGL.wasm", // URL del código
   });
 
   const handleLogout = () => {
     logout(); // Llama a la función de logout
-    window.location.reload();
+    window.location.reload(); // Recarga la página después de cerrar sesión
   }
 
   return (
     <div>
 
-{userData ? (
+      {userData ? ( // Si hay datos del usuario, muestra el juego
+        <div> 
+          <div style={{ 
+            width: '100vw', 
+            height: '100vh', 
+            overflow: 'hidden', 
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center'
+          }}>
+            <Unity unityProvider={unityProvider} style={{ width: '1250px', height: '720px', transform: 'scale(1.0)' }} /> {/* Muestra el juego Unity WebGL */}
+          </div>
 
-  <div> 
-
-<div style={{ 
-      width: '100vw', 
-      height: '100vh', 
-      overflow: 'hidden', 
-      display: 'flex', 
-      justifyContent: 'center', 
-      alignItems: 'center'
-    }}>
-      <Unity unityProvider={unityProvider} style={{ width: '1250px', height: '720px', transform: 'scale(1.0)' }} />
-    </div>
-
-    
-
-    <button 
-  className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-4 rounded bg-center w-screen"
-  onClick={handleLogout}
->
-  Logout
-</button>
-      </div>
-
-      
-      
-      
-
-    
-
-
-
-) : (
-
-  <div className="flex flex-col justify-center items-center h-full">
-    <h2 className="text-3xl font-bold mb-4 text-gray-900">
-      Por favor,{" "}
-      <Link to="/" className="text-blue-500 hover:text-blue-700 transition-colors">
-        inicia sesión
-      </Link>{" "}
-      o{" "}
-      <Link to="/Registro" className="text-blue-500 hover:text-blue-700 transition-colors">
-        regístrate
-      </Link>
-      .
-    </h2>
-  </div>
-
-
-  
-)}
-
+          <button 
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-4 rounded bg-center w-screen" // Estilo del botón de logout
+            onClick={handleLogout} // Función para manejar el evento click en el botón de logout
+          >
+            Logout
+          </button>
+        </div>
+      ) : ( // Si no hay datos del usuario, muestra un mensaje para iniciar sesión o registrarse
+        <div className="flex flex-col justify-center items-center h-full">
+          <h2 className="text-3xl font-bold mb-4 text-gray-900">
+            Por favor,{" "}
+            <Link to="/" className="text-blue-500 hover:text-blue-700 transition-colors"> {/* Enlace para iniciar sesión */}
+              inicia sesión
+            </Link>{" "}
+            o{" "}
+            <Link to="/Registro" className="text-blue-500 hover:text-blue-700 transition-colors"> {/* Enlace para registrarse */}
+              regístrate
+            </Link>
+            .
+          </h2>
+        </div>
+      )}
 
     </div>
-    
   );
 }
 
-export default Game;
+export default Game; // Exporta el componente Game
