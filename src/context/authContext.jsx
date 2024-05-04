8c026c7f7db6110ee17dd665f8970c456ed4ee67
FidelBM <133674887+FidelBM@users.noreply.github.com>
2024-05-04 13:19:59 -0600

    Update authContext.jsx

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, createContext, useContext } from "react";
-import { auth } from "../firebase/firebase.config";
+import { auth } from "../firebase/firebase.config"; // Import Firebase authentication configuration and instance
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -7,10 +7,12 @@ import {
   signInWithPopup,
   signOut,
   onAuthStateChanged,
-} from "firebase/auth";
+} from "firebase/auth"; // Import Firebase authentication functions
 
+// Create authentication context
 export const authContext = createContext();
 
+// Custom hook to use authentication context
 export const useAuth = () => {
   const context = useContext(authContext);
   if (!context) {
@@ -19,21 +21,24 @@ export const useAuth = () => {
   return context;
 };
 
+// Authentication provider component
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState(""); // State to hold the authenticated user
   
+  // Effect to listen for authentication state changes
   useEffect(() => {
     const subscribed = onAuthStateChanged(auth, (currentUser) => {
       if (!currentUser) {
         console.log("no hay usuario suscrito");
-        setUser("");
+        setUser(""); // Set user state to empty if no user is authenticated
       } else {
-        setUser(currentUser);
+        setUser(currentUser); // Set user state to current user if authenticated
       }
     });
-    return () => subscribed();
+    return () => subscribed(); // Unsubscribe from onAuthStateChanged when component unmounts
   }, []);
 
+  // Function to register a new user with email and password
   const register = async (email, password) => {
     const response = await createUserWithEmailAndPassword(
       auth,
@@ -43,20 +48,25 @@ export function AuthProvider({ children }) {
     console.log(response);
   };
  
+  // Function to login with email and password
   const login = async (email, password) => {
     const response = await signInWithEmailAndPassword(auth, email, password);
     console.log(response);
   };
  
+  // Function to login with Google
   const loginWithGoogle = async () => {
     const responseGoogle = new GoogleAuthProvider();
     return await signInWithPopup(auth, responseGoogle);
   };
 
+  // Function to logout
   const logout = async () => {
     const response = await signOut(auth);
     console.log(response);
   };
+
+  // Provide the authentication context with the authentication functions and user state
   return (
     <authContext.Provider 
       value={{
