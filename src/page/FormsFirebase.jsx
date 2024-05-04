520001fc5958b2edb69b49abfeba5b8b1da1da9e
FidelBM <133674887+FidelBM@users.noreply.github.com>
2024-05-04 16:13:08 -0600

    Update FormsFirebase.jsx

--- src/page/FormsFirebase.jsx
+++ src/page/FormsFirebase.jsx
@@ -1,39 +1,54 @@
+// Autor:    Fidel Bonilla
 import React, { useState } from "react";
 import "../App.css";
 import { useAuth } from "../context/authContext";
 
 function FormsFirebase() {
   const auth = useAuth();
-  const {displayName} = auth.user
-/* A hook that allows you to use state in (formsRegister). */
+  const { displayName } = auth.user; // Extrae el nombre de usuario de la autenticación
+
+  // Hooks de estado para los campos de registro
   const [emailRegister, setEmailRegister] = useState("");
   const [passwordRegister, setPasswordRegister] = useState("");
-/* A hook that allows you to use state in t(formsLogin). */
+
+  // Hooks de estado para los campos de inicio de sesión
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  // Maneja el registro de un nuevo usuario
   const handleRegister = (e) => {
     e.preventDefault();
     auth.register(emailRegister, passwordRegister);
   };
+
+  // Maneja el inicio de sesión de un usuario existente
   const handleLogin = (e) => {
     e.preventDefault();
     auth.login(email, password);
   };
+
+  // Maneja el inicio de sesión con Google
   const handleGoogle = (e) => {
     e.preventDefault();
     auth.loginWithGoogle();
   };
+
+  // Maneja el cierre de sesión
   const handleLogout = () => {
     auth.logout();
   }
+
   return (
     <div className="App">
+      {/* Muestra el nombre de usuario si está autenticado */}
       {displayName && <h5>welcome : {displayName}</h5>}
+      
+      {/* Formulario de registro */}
       <form className="form">
         <h3 className="title">Register</h3>
         <input
           onChange={(e) => setEmailRegister(e.target.value)}
-          class="input"
+          className="input"
           type="email"
         />
         <input
@@ -45,6 +60,8 @@ function FormsFirebase() {
           submit
         </button>
       </form>
+
+      {/* Formulario de inicio de sesión */}
       <form className="form">
         <h3 className="title">Login</h3>
         <input
@@ -65,7 +82,8 @@ function FormsFirebase() {
         </button>
       </form>
 
-      <button onClick={()=> handleLogout()} className="button">Logout</button>
+      {/* Botón para cerrar sesión */}
+      <button onClick={() => handleLogout()} className="button">Logout</button>
     </div>
   );
 }
