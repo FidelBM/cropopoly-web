82a7d53414ebab72b1ed2f4f64d0d2ae8e8d4751
FidelBM <133674887+FidelBM@users.noreply.github.com>
2024-05-04 16:15:31 -0600

    Update LineChart.jsx

--- src/page/LineChart.jsx
+++ src/page/LineChart.jsx
@@ -1,3 +1,8 @@
+/*
+Autor:            Estefanía Rico
+
+El código 
+*/
 import React, { useEffect, useState } from "react"; // Importa React, useEffect y useState desde React
 import { ResponsiveLine } from "@nivo/line"; // Importa ResponsiveLine desde la librería Nivo
 
