43852e670601abd67e6f566eec783f703eb31cd8
FidelBM <133674887+FidelBM@users.noreply.github.com>
2024-05-04 21:08:17 -0600

    Update config.js

--- src/page/config.js
+++ src/page/config.js
@@ -7,4 +7,3 @@ El código son los datos de conexion al juego
 // Puerto en el que se ejecutará el servidor, obtenido de la variable de entorno LINK o por defecto el puerto 8000
 export const LINK = process.env.LINK || "https://localhost:8000";
 export const LINK_JUEGO = process.env.LINK || "https://fidelbm.github.io/cropopoly-game/";
-console.log(LINK);
