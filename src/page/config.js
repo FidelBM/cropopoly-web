c0c474fa8ee0b4d6a3e688515651e320bcca7a1d
FidelBM <133674887+FidelBM@users.noreply.github.com>
2024-05-04 21:00:22 -0600

    Update config.js

--- src/page/config.js
+++ src/page/config.js
@@ -6,3 +6,4 @@ El código son los datos de conexion al juego
 
 // Puerto en el que se ejecutará el servidor, obtenido de la variable de entorno LINK o por defecto el puerto 8000
 export const LINK = process.env.LINK || "https://localhost:8000";
+console.log(LINK);
