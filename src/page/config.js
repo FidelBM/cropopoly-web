d78b784e07511d980cbbff33e8ea78bf02488564
FidelBM <133674887+FidelBM@users.noreply.github.com>
2024-05-04 20:25:53 -0600

    Create config.js

---
+++ src/page/config.js
@@ -0,0 +1,8 @@
+/*
+Autor:            Fidel Bonilla
+
+El código son los datos de conexion al juego
+*/
+
+// Puerto en el que se ejecutará el servidor, obtenido de la variable de entorno PORT o por defecto el puerto 8000
+export const link = process.env.LINK || ;
